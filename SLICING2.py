fd35301865bdef99b175885a0ff5ca201327c70d
Ayan Mansuri (Ghost_.) <mansuriayan243@gmail.com>
2025-09-02 15:14:19 +0530

    modified:   SLICING2.py

--- SLICING2.py
+++ SLICING2.py
@@ -4,3 +4,4 @@ print(name[:3])  # output : pyt
 print(name[:])   # starting to end
 print(name[:-1])  # output : pytho
 print(name[::-1])  # output : nohtyp reverse the string
+print(name[::2])  # output : pto
